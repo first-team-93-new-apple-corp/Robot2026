6a68cea309fd8e080a4535f1366bce9997e5f166
Justin Sommer <justints890@gmail.com>
2026-01-24 14:08:22 -0600

    idk i forgot

--- src/main/java/frc/robot/Subsystems/NTSubsystem.java
+++ src/main/java/frc/robot/Subsystems/NTSubsystem.java
@@ -1,7 +1,40 @@
 package frc.robot.Subsystems;
 
-public class NTSubsystem {
-    public NTSubsystem () {
-        
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NTSubsystem extends SubsystemBase {
+    private final Field2d m_field = new Field2d();
+
+    public NTSubsystem(Pose2d robotPose, Pose2d questPose) {
+        SmartDashboard.putData("Field", m_field);
+        m_field.setRobotPose(robotPose);
+        m_field.getObject("Quest").setPose(questPose);
+    }
+
+    public NTSubsystem(Pose3d robotPose, Pose3d questPose) {
+        SmartDashboard.putData("Field", m_field);
+        m_field.setRobotPose(robotPose.toPose2d());
+        m_field.getObject("Quest").setPose(questPose.toPose2d());
+    }
+
+    public void updateRobotPose(Pose2d newPose) {
+        m_field.setRobotPose(newPose);
+    }
+
+    public void updateRobotPose(Pose3d newPose) {
+        m_field.setRobotPose(newPose.toPose2d());
+    }
+
+    public void updateQuestPose(Pose2d newPose) {
+        m_field.getObject("Quest").setPose(newPose);
+    }
+
+    public void updateQuestPose(Pose3d newPose) {
+        m_field.getObject("Quest").setPose(newPose.toPose2d());
+
     }
 }
---
+++ src/main/java/frc/robot/controls/ControllerSchemeIO.java
@@ -0,0 +1,96 @@
+package frc.robot.controls;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.TunerConstants;
+
+import static edu.wpi.first.units.Units.*;
+
+/**
+ * The Interface that provides all the controlles of our robot
+ *
+ */
+public interface ControllerSchemeIO {
+
+    public static double MaxSpeed = TunerConstants.kSpeedAt12Volts.baseUnitMagnitude();
+    public static double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
+    public static double POVDistance = .45;
+    public static double POVDistanceDiagonal = Math.sqrt(2 * (Math.pow(POVDistance, 2)));
+    public static Translation2d[] POVs = {
+            new Translation2d(0, 0), // Default
+            new Translation2d(POVDistance, 0), // Up 1
+            new Translation2d(POVDistanceDiagonal, -POVDistanceDiagonal), // up right 2
+            new Translation2d(0, -POVDistance), // Right 3
+            new Translation2d(-POVDistanceDiagonal, -POVDistanceDiagonal), // down right 4
+            new Translation2d(-POVDistance, 0), // Down 5
+            new Translation2d(-POVDistanceDiagonal, POVDistanceDiagonal), // down left 6
+            new Translation2d(0, POVDistance), // left 7
+            new Translation2d(POVDistanceDiagonal, POVDistanceDiagonal), // up left 8
+
+    };
+
+    public double InputLeft();
+
+    public double InputUp();
+
+    public double InputTheta();
+
+    public Translation2d POV();
+
+    public Trigger Seed();
+
+    public Trigger Brake();
+
+    public Trigger Menu();
+
+    public Trigger Back();
+
+    public Trigger A();
+
+    public Trigger B();
+
+    public Trigger X();
+
+    public Trigger Y();
+
+
+    public default double DriveLeft() {
+        return InputLeft();
+    }
+
+    public default double DriveUp() {
+        return InputUp();
+    }
+
+    public default double DriveTheta() {
+        return InputTheta();
+    }
+
+    public default ChassisSpeeds Speeds() {
+        return new ChassisSpeeds(DriveLeft(), DriveUp(), DriveTheta());
+    }
+
+    public default Translation2d AngleToPOV(int Angle) {
+        switch (Angle) {
+            case 0:
+                return POVs[1];
+            case 45:
+                return POVs[2];
+            case 90:
+                return POVs[3];
+            case 135:
+                return POVs[4];
+            case 180:
+                return POVs[5];
+            case 225:
+                return POVs[6];
+            case 270:
+                return POVs[7];
+            case 315:
+                return POVs[8];
+            default:
+                return POVs[0];
+        }
+    }
+}
