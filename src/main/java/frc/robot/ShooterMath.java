9a415007deb8f338bc4a00db78a1d0666816ad52
jotishk <jotishkumarsl@gmail.com>
2026-01-29 20:09:53 -0600

    Finished shooter math - tested and works

--- src/main/java/frc/robot/ShooterMath.java
+++ src/main/java/frc/robot/ShooterMath.java
@@ -2,16 +2,30 @@ package frc.robot;
 
 public class ShooterMath {
     // https://www.analyzemath.com/stepbystep_mathworksheets/parabola/parabola_3_points.html
-    public double calculateAngle(double xinit, double xfinal, double xmid, double yint, double yfinal, double ymid){
-        // double a = 
+    public static double calculateAngle(double xinit, double xfinal, double xmid, double yinit, double yfinal, double ymid) {
+        double[] point1 = { xinit, yinit };
+        double[] point2 = { xmid, ymid };
+        double[] point3 = { xfinal, yfinal };
+        // Coefficients to parabola
+        double a = calculateCoefficent(generatePartialDeterminant("a", point1, point2, point3),
+                generateTotalDeterminant(point1, point2, point3));
+        double b = calculateCoefficent(generatePartialDeterminant("b", point1, point2, point3),
+                generateTotalDeterminant(point1, point2, point3));
+        double c = calculateCoefficent(generatePartialDeterminant("c", point1, point2, point3),
+                generateTotalDeterminant(point1, point2, point3));
+
+        return Math.atan(2 * a * xinit + b);
     }
-    public double calculateV(double theta, double xfinal, double yfinal,double gravity) {
-        return (xfinal/(Math.cos(theta)*Math.sqrt((2/gravity)*(yfinal-(xfinal*Math.tan(theta))))));
+
+    public static double calculateV(double theta, double xfinal, double yfinal, double gravity) {
+        return (xfinal / (Math.cos(theta) * Math.sqrt((2 / gravity) * (yfinal - (xfinal * Math.tan(theta))))));
     }
+
     public double angleToAlign(double x1, double hubX, double y1, double hubY, double currAngle) {
-        return currAngle - Math.atan(Math.abs(hubX-y1)/Math.abs(hubY-x1));
+        return currAngle - Math.atan(Math.abs(hubX - y1) / Math.abs(hubY - x1));
     }
-    public double calculateDeterminant(double[][] matrix) {
+
+    public static double calculateDeterminantValue(double[][] matrix) {
         double a = matrix[0][0];
         double b = matrix[0][1];
         double c = matrix[0][2];
@@ -22,12 +36,41 @@ public class ShooterMath {
         double y = matrix[2][1];
         double z = matrix[2][2];
 
-        return a*q*z+b*r*x+c*p*y-a*r*y-b*p*z-c*q*x;
+        return a * q * z + b * r * x + c * p * y - a * r * y - b * p * z - c * q * x;
     }
-    public double calculateCoefficent(double[][] singleDeterminant, double[][] totalDeterminant) {
-        return calculateDeterminant(singleDeterminant)/calculateDeterminant(totalDeterminant);
-    }
-    public double calculateParabola()
 
-    return a
+    public static double calculateCoefficent(double[][] singleDeterminant, double[][] totalDeterminant) {
+        return calculateDeterminantValue(singleDeterminant) / calculateDeterminantValue(totalDeterminant);
+    }
+
+    public static double[][] generateTotalDeterminant(double[] point1, double[] point2, double[] point3) { // point is a [x,y]
+        // double
+        double[][] matrix = {
+                { Math.pow(point1[0], 2), point1[0], 1 },
+                { Math.pow(point2[0], 2), point2[0], 1 },
+                { Math.pow(point3[0], 2), point3[0], 1 },
+        };
+        return matrix;
+    }
+
+    public static double[][] generatePartialDeterminant(String partial, double[] point1, double[] point2, double[] point3) { // partial                                                                                                             // c
+        double[][] matrix = generateTotalDeterminant(point1, point2, point3);
+        if (partial.equals("a")) {
+            matrix[0][0] = point1[1];
+            matrix[1][0] = point2[1];
+            matrix[2][0] = point3[1];
+        }
+        if (partial.equals("b")) {
+            matrix[0][1] = point1[1];
+            matrix[1][1] = point2[1];
+            matrix[2][1] = point3[1];
+        }
+        if (partial.equals("c")) {
+            matrix[0][2] = point1[1];
+            matrix[1][2] = point1[1];
+            matrix[2][2] = point1[1];
+        }
+        return matrix;
+    }
 }
+
