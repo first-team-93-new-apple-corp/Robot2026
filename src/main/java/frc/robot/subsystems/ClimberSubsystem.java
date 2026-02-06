28b76f036852ea3fbadafc2f124f718d090a0f01
funk jr. <atschwoerer@gmail.com>
2026-02-05 20:06:46 -0600

    Attempting to fix some Climber issues (and potentially causing more).

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -4,18 +4,18 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.hardware.TalonFX.DutyCycleOut;
 
 public class ClimberSubsystem extends SubsystemBase {
 
     private TalonFX climberMotor;
-    private TalonFXConfig climbermotorConfig;
+    private TalonFXConfigurator climberMotorConfig;
 
 
     public ClimberSubsystem() {
         climberMotor = new TalonFX(10);
-        m_Encoder = new DutyCycleEncoder(10);
+        // m_Encoder = new DutyCycleEncoder(10);
     }
 
     public ClimberCommands Commands = new ClimberCommands();
@@ -25,18 +25,18 @@ public class ClimberSubsystem extends SubsystemBase {
     }
 
     public void runDistance(double distance) {
-        climbermotor.setPosition(distance);
+        climberMotor.setPosition(distance);
     }
 
     public class ClimberCommands {
 
-        public Command Retract() {
-            return run(()-> setPosition(Constants.ClimberConstants.barHeight));
-        }
+        // public Command Retract() {
+        //     return run(()-> setPosition(Constants.ClimberConstants.barHeight));
+        // }
 
-        public Command Extend(){
-            return run(()-> setPosition(-Constants.ClimberConstants.barHeight));
-        }
+        // public Command Extend(){
+        //     return run(()-> setPosition(-Constants.ClimberConstants.barHeight));
+        // }
         
         public Command Stop() {
             return runOnce(() -> setSpeed(0));
@@ -54,19 +54,19 @@ public class ClimberSubsystem extends SubsystemBase {
         
         public Command autoExtendRetract() {
         // robot init, set slot 0 gains
-        m_motor.config_kF(0, 0.05, 50);
-        m_motor.config_kP(0, 0.046, 50);
-        m_motor.config_kI(0, 0.0002, 50);
-        m_motor.config_kD(0, 4.2, 50);
+        climberMotorConfig.config_kF(0, 0.05, 50);
+        climberMotorConfig.config_kP(0, 0.046, 50);
+        climberMotorConfig.config_kI(0, 0.0002, 50);
+        climberMotorConfig.config_kD(0, 4.2, 50);
 
         // enable voltage compensation
-        m_motor.configVoltageComSaturation(12);
-        m_motor.enableVoltageCompensation(true);
+        climberMotorConfig.configVoltageComSaturation(12);
+        climberMotorConfig.enableVoltageCompensation(true);
 
         // periodic, run velocity control with slot 0 configs,
         // target velocity of 50 rps (10240 ticks/100ms)
-        m_motor.selectProfileSlot(0, 0);
-        m_motor.setControl(m_motmag.withPosition(200));
+        climberMotorConfig.selectProfileSlot(0, 0);
+        climberMotorConfig.setControl(m_motmag.withPosition(200));
         }
     }
 
