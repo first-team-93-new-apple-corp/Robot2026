3a82e91a16909afc15c96e0ec85eaa200a6f0b87
funk jr. <atschwoerer@gmail.com>
2026-02-05 19:54:09 -0600

    Added Intake Commands (because they didn't merge for some reason...)

--- src/main/java/frc/robot/Subsystem/IntakeSubsytem.java
+++ src/main/java/frc/robot/Subsystem/IntakeSubsytem.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class IntakeSubsytem extends SubsystemBase {
     
@@ -21,10 +22,20 @@ public class IntakeSubsytem extends SubsystemBase {
         topIntakeMotor.set(Speed);
         bottomIntakeMotor.set(Speed);
     }
-
-    public class IntakeCommands() {
+    
+    public class IntakeCommands {
         public Command stop() {
             return runOnce(() -> setSpeed(0));
         }
+
+        public Command intake() {
+            return runOnce(() -> setSpeed(intakeSpeed));
+        }
+
+        public Command outtake() {
+            return runOnce(() -> setSpeed(outtakeSpeed));
+        }
+        
     }
+    
 }
