ded127077ef0656919bd7490d6df3fa15441fe0c
Driver2 <plexus@aasd.k12.wi.us>
2026-02-07 16:31:33 -0600

    fixed tuner constants again

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -88,7 +88,6 @@ public class TunerConstants {
     private static final boolean kInvertRightSide = true;
 
     private static final int kPigeonId = 20;
-    private static final int kPigeonId = 20;
 
     // These are only used for simulation
     private static final MomentOfInertia kSteerInertia = KilogramSquareMeters.of(0.01);
