a20d24799454c9e402e205c21d0d769dc765ca00
Justin Sommer <justints890@gmail.com>
2026-02-20 09:46:22 -0600

    no mo robot container

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
-    private final RobotContainer m_robotContainer;
+    // private final RobotContainer m_robotContainer;
 
     /* log and replay timestamp and joystick data */
     private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
@@ -21,7 +21,7 @@ public class Robot extends TimedRobot {
             .withJoystickReplay();
 
     public Robot() {
-        m_robotContainer = new RobotContainer();
+        // m_robotContainer = new RobotContainer();
     }
 
     @Override
@@ -44,7 +44,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
-        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+        m_autonomousCommand = null;
 
         if (m_autonomousCommand != null) {
             CommandScheduler.getInstance().schedule(m_autonomousCommand);
