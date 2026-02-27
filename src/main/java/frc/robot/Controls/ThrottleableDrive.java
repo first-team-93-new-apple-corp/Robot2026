9f55745a9d96981ded8ef843ee94a7abc8a45480
LavaWolf911 <@connorw911@gmail.com>
2026-02-26 20:10:31 -0600

    Fixed Controller Scheme

--- src/main/java/frc/robot/Controls/ThrottleableDrive.java
+++ src/main/java/frc/robot/Controls/ThrottleableDrive.java
@@ -1,25 +1,24 @@
+package frc.robot.Controls;
 
-// package frc.robot.Controls;
+public class ThrottleableDrive extends TwoStickDriveXboxOp{
+    public ThrottleableDrive(int LeftPort, int RightPort, int opPort){
+        super(LeftPort, RightPort, opPort);
+    }
+    private double Speedthrottle(){
+        return 1-LeftStick.getThrottle();
+    }
+    @Override
+    public double InputLeft() {
+        return deadzone(-LeftStick.getY() )* Speedthrottle();
+    }
 
-// public class ThrottleableDrive extends TwoStickDriveXboxOp{
-//     public ThrottleableDrive(int LeftPort, int RightPort, int opPort){
-//         super(LeftPort, RightPort, opPort);
-//     }
-//     private double Speedthrottle(){
-//         return 1-LeftStick.getThrottle();
-//     }
-//     @Override
-//     public double InputLeft() {
-//         return deadzone(-LeftStick.getY() )* Speedthrottle();
-//     }
+    @Override
+    public double InputUp() {
+        return deadzone(-LeftStick.getX() ) * Speedthrottle();
+    }
 
-//     @Override
-//     public double InputUp() {
-//         return deadzone(-LeftStick.getX() ) * Speedthrottle();
-//     }
-
-//     @Override
-//     public double InputTheta() {
-//         return deadzone(-RightStick.getX() ) * Speedthrottle();
-//     }
-// }
+    @Override
+    public double InputTheta() {
+        return deadzone(-RightStick.getX() ) * Speedthrottle();
+    }
+}
--- src/main/java/frc/robot/Controls/XboxDrive.java
+++ src/main/java/frc/robot/Controls/XboxDrive.java
@@ -1,15 +1,139 @@
-// package frc.robot.Controls;
+package frc.robot.Controls;
 
-// import frc.robot.Controls.ControllerSchemeIO;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants;
 
-// import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+public class XboxDrive implements ControllerSchemeIO {
 
-// public class XboxDrive implements ControllerSchemeIO {
+    public CommandXboxController Xbox;
 
-//     public CommandXboxController Xbox;
+    public XboxDrive(int port) {
+        Xbox = new CommandXboxController(port);
+    }
 
-//     public XboxDrive(int port) {
-//         Xbox = new CommandXboxController(port);
-//     }
+    public double deadzone(double value) {
+        if (Math.abs(value) < Constants.Thrustmaster.Deadzone) {
+            return 0.0;
+        }
+        return value;
+    }
 
-// }
+    @Override
+    public double InputLeft() {
+        return deadzone(-Xbox.getLeftY());
+    }
+
+    @Override
+    public double InputUp() {
+        return deadzone(-Xbox.getLeftX());
+    }
+
+    @Override
+    public double InputTheta() {
+        return deadzone(-Xbox.getRightX());
+    }
+
+    @Override
+    public Trigger Intake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Intake'");
+    }
+
+    @Override
+    public Trigger Outtake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Outtake'");
+    }
+
+    @Override
+    public Trigger Shoot() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Shoot'");
+    }
+
+    @Override
+    public Trigger manRetractClimber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'manRetractClimber'");
+    }
+
+    @Override
+    public Trigger manExtendClimber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'manExtendClimber'");
+    }
+
+    @Override
+    public Trigger autoRetractClimber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'autoRetractClimber'");
+    }
+
+    @Override
+    public Trigger autoExtendClimber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'autoExtendClimber'");
+    }
+
+    @Override
+    public Trigger WiggleIntake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'WiggleIntake'");
+    }
+
+    @Override
+    public Trigger LowerIntake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'LowerIntake'");
+    }
+
+    @Override
+    public Trigger RaiseIntake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'RaiseIntake'");
+    }
+
+    @Override
+    public Trigger baseIntake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'baseIntake'");
+    }
+
+    @Override
+    public Trigger maxIntake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'maxIntake'");
+    }
+
+    @Override
+    public Trigger middleIntake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'middleIntake'");
+    }
+
+    @Override
+    public Trigger alignShooter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'alignShooter'");
+    }
+
+    @Override
+    public Trigger seed() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'seed'");
+    }
+
+    @Override
+    public Trigger brake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'brake'");
+    }
+
+    @Override
+    public Trigger robotRel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'robotRel'");
+    }
+}
