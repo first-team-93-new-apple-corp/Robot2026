bed62ba8c5bc073d6d04820f677d8b02b1804600
LavaWolf911 <@connorw911@gmail.com>
2026-03-21 13:41:34 -0500

    autos

--- src/main/java/frc/robot/Subsystems/auto/AutoDirector.java
+++ src/main/java/frc/robot/Subsystems/auto/AutoDirector.java
@@ -300,7 +300,7 @@ public class AutoDirector {
     public Auto DepotScoreCenter() {
         Pose2d startPose = autoSubsystems.drivetrain().getPose();
         AutoTracker tracker = new AutoTracker(autoSubsystems, startPose);
-        tracker.addIntakePath("depotIntake", MetersPerSecond.of(0.1));
+        tracker.addIntakePath("depotIntake");
         tracker.addShootPath("Shoot Center");
         tracker.endAuto();
         return new Auto("IntakeDepotScore", tracker, startPose);
@@ -308,7 +308,7 @@ public class AutoDirector {
     public Auto DepotScoreCenterLeft() {
         Pose2d startPose = autoSubsystems.drivetrain().getPose();
         AutoTracker tracker = new AutoTracker(autoSubsystems, startPose);
-        tracker.addIntakePath("depotIntake", MetersPerSecond.of(0.1));
+        tracker.addIntakePath("depotIntake");
         tracker.addShootPath("Shoot Center");
         tracker.endAuto();
         return new Auto("IntakeDepotScore", tracker, startPose);
@@ -316,16 +316,27 @@ public class AutoDirector {
     public Auto DepotScoreCenterRight() {
         Pose2d startPose = autoSubsystems.drivetrain().getPose();
         AutoTracker tracker = new AutoTracker(autoSubsystems, startPose);
-        tracker.addIntakePath("depotIntake", MetersPerSecond.of(0.1));
+        tracker.addIntakePath("depotIntake");
         tracker.addShootPath("Shoot Center Right");
         tracker.endAuto();
         return new Auto("IntakeDepotScore", tracker, startPose);
     }
 
+    public Auto OverBumpLeftShootCenter(){
+        AutoTracker tracker = new AutoTracker(autoSubsystems);
+        tracker.addOverBumpLeft("Overbump");
+        tracker.addIntakeChoreo("L_Center_Intake");
+        tracker.addCommands(autoSubsystems.intake().commands.stop());
+        tracker.addOverBumpLeft("BakcOverbump");
+        tracker.addShootPathCenter("Shoot Center");
+        tracker.endAuto();
+        return new Auto("CenterShootCenter", tracker);
+    }
+
     public Auto PreloadOutpost() {
         Pose2d startPose = autoSubsystems.drivetrain().getPose();
         AutoTracker tracker = new AutoTracker(autoSubsystems, startPose);
-        tracker.addIntakePath("depotIntake", MetersPerSecond.of(0.1));
+        tracker.addIntakePath("depotIntake");
         if (getAlliance() == Alliance.Red) {
             tracker.goToAndThenShootClose(()->new Pose2d(autoSubsystems.drivetrain().getState().Pose.getX() - 1,
                     autoSubsystems.drivetrain().getState().Pose.getY() - 1,
