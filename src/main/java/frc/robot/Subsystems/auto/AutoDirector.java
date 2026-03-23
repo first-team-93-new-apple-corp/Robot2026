7ffa46a0cd61409be910388296bd22c4ba304a88
Justin Sommer <justints890@gmail.com>
2026-03-23 10:35:22 -0500

    Revert "removed duplicate methods from merge"
    This reverts commit 58e92dda38903ab24ce0a90f8d1ed04c8df7dfce.

--- src/main/java/frc/robot/Subsystems/auto/AutoDirector.java
+++ src/main/java/frc/robot/Subsystems/auto/AutoDirector.java
@@ -323,7 +323,7 @@ public class AutoDirector {
     public Auto DepotScoreCenterLeft() {
         Pose2d startPose = autoSubsystems.drivetrain().getPose();
         AutoTracker tracker = new AutoTracker(autoSubsystems, startPose);
-        tracker.addIntakePath("depotIntake");
+        tracker.addIntakePath("depotIntake", MetersPerSecond.of(0.1));
         tracker.addShootPath("Shoot Center Left");
         tracker.endAuto();
         return new Auto("Depot Shoot Center Left", tracker, startPose);
