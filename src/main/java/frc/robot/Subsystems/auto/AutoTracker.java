1cf2c396d870dd6086ac9d4c28fc6999524e1ae8
Driver1 <plexus@aasd.k12.wi.us>
2026-03-26 20:23:03 -0500

    added todo line and commented out untested auto stuffs

--- src/main/java/frc/robot/Subsystems/auto/AutoTracker.java
+++ src/main/java/frc/robot/Subsystems/auto/AutoTracker.java
@@ -236,9 +236,10 @@ public class AutoTracker extends SequentialCommandGroup {
     }
 
     private void overBump(PathPlannerPath path) {
-        // addCommands(AutoBuilder.pathfindThenFollowPath(path, PathConstraints.unlimitedConstraints(12)));
-        addCommands(AutoBuilder.pathfindToPose(path.getStartingHolonomicPose().get(), PathConstraints.unlimitedConstraints(12), MetersPerSecond.of(1)));
-        addCommands(AutoBuilder.followPath(path));
+        addCommands(AutoBuilder.pathfindThenFollowPath(path, PathConstraints.unlimitedConstraints(12)));
+        //TODO Never got to test this
+        // addCommands(AutoBuilder.pathfindToPose(path.getStartingHolonomicPose().get(), PathConstraints.unlimitedConstraints(12), MetersPerSecond.of(1)));
+        // addCommands(AutoBuilder.followPath(path));
         // addCommands(Commands.runOnce(() -> subsystems.drivetrain().snapToPose(AutoConstants.getLastPoseInPath(path)))
         //         .withTimeout(3));
     }
