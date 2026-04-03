6dfcec1510dfcbc88ab3a38e69d8cf1ae70a3c24
LavaWolf911 <@connorw911@gmail.com>
2026-04-03 14:23:23 -0500

    fixed autos (wrong path names

--- src/main/java/frc/robot/Subsystems/auto/AutoDirector.java
+++ src/main/java/frc/robot/Subsystems/auto/AutoDirector.java
@@ -267,7 +267,7 @@ public class AutoDirector {
 
     public Auto OutpostScoreClose() {
         AutoTracker tracker = new AutoTracker(autoSubsystems);
-        tracker.addIntakePath("depotIntake");
+        tracker.addIntakePath("outpostIntake");
         tracker.addShootPath("Shoot Center", Presets.close);
         tracker.endAuto();
         return trackedAuto("Outpost Score Center", tracker);
@@ -275,7 +275,7 @@ public class AutoDirector {
 
     public Auto OutpostScoreCloseLeft() {
         AutoTracker tracker = new AutoTracker(autoSubsystems);
-        tracker.addIntakePath("depotIntake");
+        tracker.addIntakePath("outpostIntake");
         tracker.addShootPath("Shoot Center Left", Presets.closeSide);
         tracker.endAuto();
         return trackedAuto("Outpost Score Center Left", tracker);
@@ -283,7 +283,7 @@ public class AutoDirector {
 
     public Auto OutpostScoreCloseRight() {
         AutoTracker tracker = new AutoTracker(autoSubsystems);
-        tracker.addIntakePath("depotIntake");
+        tracker.addIntakePath("outpostIntake");
         tracker.addShootPath("Shoot Center Right", Presets.closeSide);
         tracker.endAuto();
         return trackedAuto("Outpost Score Center Right", tracker);
