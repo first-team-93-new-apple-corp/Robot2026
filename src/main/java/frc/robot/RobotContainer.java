ebc52abe57e46c75a342a1b0e8f91ef8bf024f50
Justin Sommer <justints890@gmail.com>
2026-04-16 18:54:06 -0500

    are we done yet?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -49,10 +49,7 @@ public class RobotContainer {
 
         private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
 
-        // private final Telemetry logger = new Telemetry(MaxSpeed);
-
         private final ControllerSchemeIO driver = new TwoStickDriveXboxOp(0, 1, 2);
-        // private final ControllerSchemeIO driver = new XboxDrive(2);
 
         public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
@@ -149,11 +146,9 @@ public class RobotContainer {
                                 .autoAngleNoOffset(() -> Degrees.of(driver.rightTrigger()).times(18)).repeatedly());
                 driver.manHood().onFalse(subsystems.shooter().commands.autoAngleNoOffset(Degrees.of(0)));
 
-                // RobotModeTriggers.autonomous().onTrue(subsystems.vision().commands.resetPose().ignoringDisable(true));
                 RobotModeTriggers.autonomous()
                                 .onTrue(Commands.runOnce(() -> Elastic.selectTab(1)).ignoringDisable(true));
                 RobotModeTriggers.teleop().onTrue(Commands.runOnce(() -> Elastic.selectTab(0)).ignoringDisable(true));
-                // RobotModeTriggers.teleop().onTrue(subsystems.vision().commands.resetPose().ignoringDisable(true));
                 (new Trigger(() -> auto.hasSelectedAutoPreviewChanged()).and(RobotModeTriggers.disabled()))
                                 .onTrue(Commands.runOnce(auto::updateSelectedAutoPreview).ignoringDisable(true));
                 RobotModeTriggers.teleop().onTrue(Commands.runOnce(auto::removePreview));
