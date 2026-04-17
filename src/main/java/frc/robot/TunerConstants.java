c0d761bb47245bc5d77365ea552274823ef9f324
Justin Sommer <justints890@gmail.com>
2026-04-17 16:00:35 -0500

    Bring back old sysid values

--- src/main/java/frc/robot/TunerConstants.java
+++ src/main/java/frc/robot/TunerConstants.java
@@ -30,8 +30,8 @@ public class TunerConstants {
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0)
-        .withKS(0).withKV(0.124);
+        .withKP(0.17454).withKI(0).withKD(0)
+        .withKS(0.15966).withKV(0.116).withKA(0.011018);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
